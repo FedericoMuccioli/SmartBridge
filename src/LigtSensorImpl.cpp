2ed3ad1d8eaf58ea0ef753ac68aecd3390c9dd07
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-11-26 19:53:45 +0100

    Create LigtSensorImpl.cpp

---
+++ src/LigtSensorImpl.cpp
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+#include "LightSensorImpl.h"
+
+LightSensorImpl::LightSensorImpl(int pin){
+  this->pin = pin;
+  pinMode(pin,INPUT);
+}
+
+int LightSensorImpl::getLightIntensity(){
+  return analogRead(pin);
+}
