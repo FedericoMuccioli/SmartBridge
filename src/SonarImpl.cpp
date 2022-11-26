46b2029b074cc976fcfe01959559400ff60fe982
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-11-26 19:54:29 +0100

    Create SonarImpl.cpp

---
+++ src/SonarImpl.cpp
@@ -0,0 +1,31 @@
+#include "Arduino.h"
+#include "SonarImpl.h"
+
+const float soundSpeed = 331.45 + 0.62*20; //supponendo di eseguire il test in un ambiente a 20 °C
+
+SonarImpl::SonarImpl(int trigPin, int echoPin){
+  this->trigPin = trigPin;
+  this->echoPin = echoPin;
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+}
+
+float SonarImpl::getDistance(){
+  sendImpulse();
+  return getTimeImpulse()*soundSpeed;
+}
+
+void SonarImpl::sendImpulse(){
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(3);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin,LOW);
+}
+
+float SonarImpl::getTimeImpulse(){
+  float tUS = pulseIn(echoPin, HIGH);
+  return tUS / 1000.0 / 1000.0 / 2;
+}
+
+
