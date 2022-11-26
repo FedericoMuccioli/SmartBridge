250ed0541dd74223133bfc0059dafec35fe5bc04
FedericoMuccioli <federicomuccioliabc@gmail.com>
2022-11-26 19:54:06 +0100

    Create PirSensorImpl.cpp

---
+++ src/PirSensorImpl.cpp
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+#include "PirSensorImpl.h"
+
+PirSensorImpl::PirSensorImpl(int pin){
+  this->pin = pin;
+  pinMode(pin,INPUT);
+}
+
+bool PirSensorImpl::isMoved(){
+  return digitalRead(pin) == HIGH;
+}
